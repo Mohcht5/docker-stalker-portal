456b01a0eb719679eaf5b7b4cc8a95618d88f17f
Mohcht5 <115098001+Mohcht5@users.noreply.github.com>
2024-12-10 22:05:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,50 +1,24 @@
 #!/bin/bash
 
-echo "If you are using external DB please do the following inside the container (docker exec -it stalker-portal bash):"
-echo "1. If the DB contents do not match the version of the container - run: docker exec -it stalker-portal sh -c 'cd /var/www/stalker_portal/deploy/ && phing'"
-echo "2. Load the TZ info once: docker exec -it stalker-portal sh -c 'mysql_tzinfo_to_sql /usr/share/zoneinfo 2>/dev/null | /usr/bin/mysql -u root -p mysql -h DB_HOST"
-echo "3. On the external DB set max_allowed_packet = 32M in /etc/mysql/my.cnf"
+echo "Starting Ministra/Stalker Portal setup for Koyeb..."
 
+# نسخ ملفات التكوين
 cp -f /opt/conf/nginx/*.conf /etc/nginx/conf.d/
-cp -f /opt/conf/apache2/*.conf /etc/apache2/sites-available/
-cp -f /opt/conf/apache2/conf-available/*.conf /etc/apache2/conf-available/
 cp -f /opt/conf/custom.ini /var/www/stalker_portal/server/
 
-if [ -n ${TZ} ]; then
- ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+# ضبط المنطقة الزمنية
+if [ -n "${TZ}" ]; then
+  ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 fi
 
-if [ $(ls /var/lib/mysql|wc -l) -eq 0 ]
-then
- echo "Copy default DB schema to /var/lib/mysql"
- cp -rf /root/mysql_default/* /var/lib/mysql/
- chown -R mysql:mysql /var/lib/mysql
-fi
+# التأكد من تشغيل Nginx
+service nginx start
 
-service mysql start
-if [ $(ls /opt/conf/mysql/*.sql | wc -l) -eq 1 ]
-then
- echo "Restoring dump to stalker_db database..."
- sqldump=$(ls -t /opt/conf/mysql/*.sql|head -n 1)
- mysql -u stalker -p1 -e 'drop database stalker_db;'
- mysql -u stalker -p1 -e 'create database stalker_db;'
- mysql -u stalker -p1 stalker_db < $sqldump
- rm -f $sqldump
-fi
-
-services=(mysql memcached cron apache2 nginx)
+echo "Setup completed. Starting Nginx server..."
 while true; do
- for service in ${services[@]}; do
-  if [ $(pgrep $service | wc -l) -eq 0 ]
-  then
-   echo "Service $service is not running. Starting"
-   if [ "$service" != "cron" ]
-   then
-    service $service start
-   else
-    cron
-   fi
+  if [ $(pgrep nginx | wc -l) -eq 0 ]; then
+    echo "Nginx is not running. Starting..."
+    service nginx start
   fi
- done
- sleep 5
+  sleep 5
 done
