1ee16cdcf8e6de9a3452820c094b4a50fb27b6b2
Mohcht5 <115098001+Mohcht5@users.noreply.github.com>
2024-12-10 22:14:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
-echo "Starting Ministra/Stalker Portal setup for Koyeb..."
-
-# نسخ ملفات التكوين
+# إعداد ملفات التهيئة
 cp -f /opt/conf/nginx/*.conf /etc/nginx/conf.d/
+cp -f /opt/conf/apache2/*.conf /etc/apache2/sites-available/
+cp -f /opt/conf/apache2/conf-available/*.conf /etc/apache2/conf-available/
 cp -f /opt/conf/custom.ini /var/www/stalker_portal/server/
 
 # ضبط المنطقة الزمنية
 if [ -n "${TZ}" ]; then
-  ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+    echo $TZ > /etc/timezone
 fi
 
-# التأكد من تشغيل Nginx
-service nginx start
+# استعادة قاعدة البيانات الافتراضية
+if [ $(ls /var/lib/mysql | wc -l) -eq 0 ]; then
+    echo "Copying default DB schema to /var/lib/mysql"
+    cp -rf /root/mysql_default/* /var/lib/mysql/
+    chown -R mysql:mysql /var/lib/mysql
+fi
 
-echo "Setup completed. Starting Nginx server..."
+# بدء MySQL وإعداد القاعدة إذا لزم الأمر
+service mysql start
+if [ $(ls /opt/conf/mysql/*.sql | wc -l) -eq 1 ]; then
+    echo "Restoring dump to stalker_db database..."
+    sqldump=$(ls -t /opt/conf/mysql/*.sql | head -n 1)
+    mysql -u stalker -p1 -e 'DROP DATABASE stalker_db;'
+    mysql -u stalker -p1 -e 'CREATE DATABASE stalker_db;'
+    mysql -u stalker -p1 stalker_db < $sqldump
+    rm -f $sqldump
+fi
+
+# تشغيل الخدمات الضرورية
+services=(mysql memcached cron apache2 nginx)
 while true; do
-  if [ $(pgrep nginx | wc -l) -eq 0 ]; then
-    echo "Nginx is not running. Starting..."
-    service nginx start
-  fi
-  sleep 5
+    for service in "${services[@]}"; do
+        if [ $(pgrep $service | wc -l) -eq 0 ]; then
+            echo "Service $service is not running. Starting it..."
+            if [ "$service" != "cron" ]; then
+                service $service start
+            else
+                cron
+            fi
+        fi
+    done
+    sleep 5
 done
